573ea079543824ad652b0a6e82a3dada4081199d
kevn7n <kevn_nguyen@yahoo.com>
2021-04-22 17:12:48 -0600

    Create routes, dishes nested in chefs

--- app/models/dish_ingredient.rb
+++ app/models/dish_ingredient.rb
@@ -1,4 +1,7 @@
 class DishIngredient <ApplicationRecord
+  # validates :dish_id, presence: true
+  # validates :ingredient_id, presence: true
+  
   belongs_to :dish
   belongs_to :ingredient
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'chefs#index'
+
+resources :chefs, only: [:index, :show] do
+  resources :dishes, only: [:index, :show]
+end
+resources :ingredients, only: [:index, :show]
+
 end
