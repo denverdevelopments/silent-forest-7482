82ac6ab9a6e4fa0a8be646f507a27202c863eae2
kevn7n <kevn_nguyen@yahoo.com>
2021-04-22 17:48:14 -0600

    Create dish, ingredient spec model tests, passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'chefs#index'
+root 'ingredients#show'
 
-resources :chefs, only: [:index, :show] do
-  resources :dishes, only: [:index, :show]
+resources :chefs, only: [:show] do
+  resources :dishes, only: [:destroy]
 end
-resources :ingredients, only: [:index, :show]
+resources :ingredients, only: [:show]
 
 end
