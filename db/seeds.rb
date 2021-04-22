3255989df1c59ba1525af0011ec09943b2898eee
kevn7n <kevn_nguyen@yahoo.com>
2021-04-22 17:00:43 -0600

    Create seeds.rb, db populates

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,26 @@ DishIngredient.destroy_all
 
 pat = Chef.create!(name: "Pat Smith")
 jill = Chef.create!(name: "Jill Jones")
+
+coucous = pat.dishes.create!(name: "Coucous", description: "Healthy grain")
+lamb = pat.dishes.create!(name: "Farm Lamb", description: "Savory protein")
+pilaf = Dish.create!(name: "Pilaf", description: "Wild rices", chef_id: jill.id)
+crispy = Dish.create!(name: "Crispy Chicken", description: "Fine fowl", chef_id: jill.id)
+
+salt = Ingredient.create!(name: "Salt", calories: 3)
+butter = Ingredient.create!(name: "Butter", calories: 102)
+oil = Ingredient.create!(name: "Olive Oil", calories: 250)
+grain = Ingredient.create!(name: "Grain", calories: 37)
+
+coucous.ingredients << butter
+coucous.ingredients << salt
+coucous.ingredients << grain
+
+lamb.ingredients << oil
+lamb.ingredients << butter
+
+DishIngredient.create!(dish: pilaf, ingredient: grain)
+DishIngredient.create!(dish: pilaf, ingredient: oil)
+
+DishIngredient.create!(dish: crispy, ingredient: oil)
+DishIngredient.create!(dish: crispy, ingredient: butter)
